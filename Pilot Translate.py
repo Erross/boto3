299e9368c5f52c197f06ceb39242faf1cc510dfd
EWAN ROSS <erross@monsanto.com>
2018-09-28 09:33:40 -0500

    add region to translate

--- Pilot Translate.py
+++ Pilot Translate.py
@@ -17,7 +17,8 @@ def onProcess(ctxt, dr):
 
     translate = boto3.client('translate',
                              aws_access_key_id='$(Access_Key)',
-                             aws_secret_access_key='$(Secret_Key)'
+                             aws_secret_access_key='$(Secret_Key)',
+                             region_name='$(Region)'
                              )
 
     response = translate.translate_text(
