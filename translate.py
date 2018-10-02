47cfcf89adb840f5666cd720f01c5f51af289126
EWAN ROSS <erross@monsanto.com>
2018-10-02 15:39:56 -0500

    Updating translator

--- translate.py
+++ translate.py
@@ -1,15 +1,14 @@
 import boto3
 
 translate = boto3.client('translate',
-    aws_access_key_id='$(Access_Key)',
-    aws_secret_access_key='$(Secret_Key')
-
-    )
+aws_access_key_id='$(Access_Key)',
+aws_secret_access_key='$(Secret_Key'),
+region_name='$(Region)')
 
 response = translate.translate_text(
-    Text='你好,世界',
-    SourceLanguageCode='Auto',
-    TargetLanguageCode='En'
+Text='你好,世界',
+SourceLanguageCode='Auto',
+TargetLanguageCode='En'
 )
 
 print(response['TranslatedText'])
